e2d8544fde088ac53e204f42858c689ef8d12722
khalilelrifai <118453256+khalilelrifai@users.noreply.github.com>
2022-12-22 20:46:02 +0200

    commit

--- apps/home/views.py
+++ apps/home/views.py
@@ -30,6 +30,14 @@ def pages(request):
         if load_template == 'admin':
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
+        
+        
+        
+        
+        
+        
+        
+        
 
         html_template = loader.get_template('home/' + load_template)
         return HttpResponse(html_template.render(context, request))
