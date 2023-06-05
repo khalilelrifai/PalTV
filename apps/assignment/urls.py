17785c774cbb48030580f260b99dbd4f62923883
khalilelrifai <118453256+khalilelrifai@users.noreply.github.com>
2023-06-05 21:27:17 +0300

    commit

--- apps/assignment/urls.py
+++ apps/assignment/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import upload_video, video_list
 
+app_name='assignment'
+
 urlpatterns = [
     path('upload/', upload_video, name='upload_video'),
     path('list/', video_list, name='video_list'),
