476b3f063c5fad18521e2fa125196db0409cf76e
khalilelrifai <118453256+khalilelrifai@users.noreply.github.com>
2023-06-07 21:24:50 +0300

    commit

--- apps/assignment/models.py
+++ apps/assignment/models.py
@@ -1,9 +1,24 @@
 from django.db import models
+from ftplib import FTP
+from django.conf import settings
+from django.db.models import *
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     file = models.FileField(upload_to='videos/')
+    upload_status = models.CharField(max_length=100, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    ftp_exists = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    def check_ftp_exists(self):
+        ftp = FTP(settings.FTP_HOST)
+        ftp.login(user=settings.FTP_USER, passwd=settings.FTP_PASSWORD)
+        ftp.cwd(settings.FTP_UPLOAD_DIR)
+        file_list = ftp.nlst()
+        self.ftp_exists = self.file.name in file_list
+        self.save()
+        ftp.quit()
--- apps/assignment/views.py
+++ apps/assignment/views.py
@@ -1,30 +1,46 @@
 from django.shortcuts import render, redirect
 from .forms import VideoForm
 from ftplib import FTP
-from .models import *
 from django.conf import settings
+from .models import *
 
 def upload_video(request):
     if request.method == 'POST':
         form = VideoForm(request.POST, request.FILES)
-        
         if form.is_valid():
-            video = form.save()
-            # Upload the video file to the FTP server
-            ftp = FTP(settings.FTP_HOST)
-            ftp.login(user=settings.FTP_USER, passwd=settings.FTP_PASSWORD)
-            ftp.cwd(settings.FTP_UPLOAD_DIR)
-            ftp.set_pasv(False)
-            print( ftp.pwd())
-
-            with open(video.file.path, 'rb') as file:
-                ftp.storbinary(f'STOR {video.file.name}', file)
-            
-            ftp.quit()
-            return redirect('assignment:video_list')
+            video = form.save(commit=False)
+            video.upload_status = 'Uploading...'
+            video.save()
+            try:
+                # Upload the video file to the FTP server
+                ftp = FTP(settings.FTP_HOST)
+                ftp.login(user=settings.FTP_USER, passwd=settings.FTP_PASSWORD)
+                ftp.cwd(settings.FTP_UPLOAD_DIR)
+                with open(video.file.path, 'rb') as file:
+                    ftp.storbinary(f'STOR {video.file.name}', file)
+                ftp.quit()
+                video.upload_status = 'Uploaded successfully!'
+                video.check_ftp_exists()
+                return redirect('assignment:video_list')
+            except Exception as e:
+                video.upload_status = f'Upload failed: {str(e)}'
+                video.check_ftp_exists()
+                video.save()
+                return redirect('assignment:video_list')
     else:
         form = VideoForm()
     return render(request, 'assignment/upload.html', {'form': form})
+
+
+def video_list(request):
+    videos = Video.objects.all()
+    for video in videos:
+        if video.upload_status == 'Uploaded successfully!':
+            video.check_ftp_exists()
+    return render(request, 'videos/list.html', {'videos': videos})
+
+
+
 
 def video_list(request):
     videos = Video.objects.all()
