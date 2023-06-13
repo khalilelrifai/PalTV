65ff148bc6bb2cb4a55ef852fb2624c002159ce7
khalilelrifai <118453256+khalilelrifai@users.noreply.github.com>
2023-06-13 14:44:17 +0300

    commit

--- apps/assignment/models.py
+++ apps/assignment/models.py
@@ -3,7 +3,7 @@ from ftplib import FTP
 from django.conf import settings
 from django.db import models
 from django.db.models import *
-
+from django.contrib.auth.models import User
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
@@ -11,6 +11,7 @@ class Video(models.Model):
     upload_status = models.CharField(max_length=100, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     ftp_exists = models.BooleanField(default=False)
+    owner = ForeignKey(User,on_delete=SET_NULL,null=True)
 
     def __str__(self):
         return self.title
@@ -23,7 +24,6 @@ class Video(models.Model):
             file_name = self.title
             files_List = [file_name.replace('videos/', '').split('.')[0] for file_name in ftp.nlst('videos/')]
             file_exists = file_name in files_List
-            print(files_List)
             self.ftp_exists = file_exists
             self.save()
             ftp.quit()
--- apps/assignment/views.py
+++ apps/assignment/views.py
@@ -6,54 +6,61 @@ from .models import *
 
 import os
 
+from django.core.files.base import ContentFile
+
 def upload_video(request):
     if request.method == 'POST':
         form = VideoForm(request.POST, request.FILES)
         if form.is_valid():
             video = form.save(commit=False)
+            video.owner = request.user
             video.upload_status = 'Uploading...'
             video.save()
-            
+
             try:
                 # Upload the video file to the FTP server
                 ftp = FTP(settings.FTP_HOST)
                 ftp.login(user=settings.FTP_USER, passwd=settings.FTP_PASSWORD)
                 ftp.cwd(settings.FTP_UPLOAD_DIR)
-                
+
                 # Get the title from the form
                 title = form.cleaned_data['title']
                 file = request.FILES['file']
                 # Generate new file name using the title
                 new_file_name = f'{title}.mp4'
-                
-                ftp.cwd('videos')
-                
 
-                    # Use the new file name for FTP storage
-                ftp.storbinary(f'STOR {new_file_name}', file)
+                ftp.cwd('videos')
+
+                # Read the file content and upload it to the FTP server
+                file_content = file.read()
+                ftp.storbinary(f'STOR {new_file_name}', ContentFile(file_content))
                 ftp.quit()
-                
+
                 # Update the video object with the new file name
-                video.file.name = new_file_name
                 video.upload_status = 'Uploaded successfully!'
+                video.file = new_file_name
                 video.check_ftp_exists()
                 video.save()
-                
+
                 return redirect('assignment:video_list')
-                
+
             except Exception as e:
                 video.upload_status = f'Upload failed: {str(e)}'
                 video.check_ftp_exists()
                 video.save()
                 return redirect('assignment:video_list')
-                
+
     else:
         form = VideoForm()
-        
+
     return render(request, 'assignment/upload.html', {'form': form})
 
+
+
+
+
 def video_list(request):
-    videos = Video.objects.filter(upload_status='Uploaded successfully!')
+    videos = Video.objects.all()
     
     for video in videos:
         video.check_ftp_exists()
