98e7f65f27605a81e785b86d4acad5f9429857d4
khalilelrifai <118453256+khalilelrifai@users.noreply.github.com>
2023-06-26 16:09:47 +0300

    commit

--- apps/assignment/urls.py
+++ apps/assignment/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import upload_video, video_list
+from .views import *
 from django.contrib.auth.decorators import login_required
 
 app_name='assignment'
 
 
 urlpatterns = [
-    path('', login_required(video_list), name='main'),
+    path('', login_required(VideoListView.as_view()), name='main'),
     path('upload/',login_required(upload_video), name='upload_video'),
-    path('list/', login_required(video_list), name='video_list'),
+    path('list/', login_required(VideoListView.as_view()), name='video_list'),
 ]
