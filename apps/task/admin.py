7f580e932229596b4252d19496ea9146a0149181
khalilelrifai <118453256+khalilelrifai@users.noreply.github.com>
2023-12-05 14:36:49 +0200

    commit

--- apps/task/admin.py
+++ apps/task/admin.py
@@ -2,12 +2,14 @@
 
 from django.contrib import admin
 
-from .models import  Role, Task
+from .models import  *
 
 
 
 
 admin.site.register(Task)
-
+admin.site.register(Guest)
+admin.site.register(GuestBooker)
+admin.site.register(GuestCSV)
 admin.site.register(Role)
 
